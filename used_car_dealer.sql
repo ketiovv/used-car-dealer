4ad56000b15e36c2c89357de4e66bc739b8f126c
ketiovv <wojtek.kajstura@gmail.com>
2020-06-18 00:41:46 +0200

    start working with project

---
+++ used_car_dealer.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS used_car_dealer;
+CREATE DATABASE used_car_dealer;
+USE used_car_dealer;
+
+DROP TABLE IF EXISTS car_manufacturer;
+#CREATE TABLE car_manufacturer
+
+DROP TABLE IF EXISTS car_model;
+#CREATE TABLE car_model
+
+DROP TABLE IF EXISTS car;
+#CREATE TABLE car

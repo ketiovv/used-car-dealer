4102044b341c444d37e1e07c875b43017b49ce3f
ketiovv <wojtek.kajstura@gmail.com>
2020-07-09 21:14:02 +0200

    add vs solution

---
+++ used_car_dealer/used_car_dealer/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using used_car_dealer.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace used_car_dealer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}

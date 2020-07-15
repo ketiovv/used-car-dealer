eb5f8a8b2fc744b04cc4df45982950561092c7a6
ketiovv <wojtek.kajstura@gmail.com>
2020-07-15 23:52:51 +0200

    register views for navigation

--- used_car_dealer/used_car_dealer/App.xaml.cs
+++ used_car_dealer/used_car_dealer/App.xaml.cs
@@ -2,6 +2,9 @@
 using Prism.Ioc;
 using Prism.Modularity;
 using System.Windows;
+using Prism.Mvvm;
+using used_car_dealer.ViewModels;
+using used_car_dealer.core;
 
 namespace used_car_dealer
 {
@@ -17,7 +20,13 @@ namespace used_car_dealer
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            containerRegistry.RegisterForNavigation<MainMenu>("MainMenu");
+            containerRegistry.RegisterForNavigation<CarsPanel>("CarsPanel");
+            containerRegistry.RegisterForNavigation<CustomersPanel>("CustomersPanel");
+            containerRegistry.RegisterForNavigation<DealsPanel>("DealsPanel");
 
+            containerRegistry.RegisterSingleton<IApplicationCommands, ApplicationCommands>();
         }
+
     }
 }

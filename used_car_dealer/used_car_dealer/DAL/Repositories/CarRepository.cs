f12353b87a9c350e919aec49e6df33fe21afcd59
ketiovv <wojtek.kajstura@gmail.com>
2020-07-15 23:54:23 +0200

    created repo's in dal

---
+++ used_car_dealer/used_car_dealer/DAL/Repositories/CarRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace used_car_dealer.DAL.Repositories
+{
+    class CarRepository
+    {
+    }
+}
---
+++ used_car_dealer/used_car_dealer/DAL/Repositories/CustomerRepository.cs
@@ -0,0 +1,38 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using used_car_dealer.DAL.Entities;
+
+
+namespace used_car_dealer.DAL.Repositories
+{
+    static class CustomerRepository
+    {
+        private const string allCustomersQuery = "SELECT * FROM customer";
+        private const string addCustomerQuery = "INSERT INTO 'customer'" +
+                                                "'customer_name', 'customer_lastname','customer_pesel'," +
+                                                "'customer_phone_number','customer_postal_code'," +
+                                                "'customer_city','customer_street','customer_home_number'," +
+                                                "'customer_birth_date' VALUES ";
+
+        public static List<Customer> GetAllCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+            using (var connection = DatabaseConnection.Instance.Connection)
+            {
+                var command = new MySqlCommand(allCustomersQuery, connection);
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    customers.Add(new Customer(reader));
+                }
+
+                connection.Close();
+            }
+
+            return customers;
+        }
+    }
+}

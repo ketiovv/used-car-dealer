558eba9ca8ccc642a6702bbff8c51a2bd98a018c
ketiovv <wojtek.kajstura@gmail.com>
2020-07-22 23:43:15 +0200

    deleted default values

--- used_car_dealer/used_car_dealer/ViewModels/CustomersPanelViewModel.cs
+++ used_car_dealer/used_car_dealer/ViewModels/CustomersPanelViewModel.cs
@@ -31,63 +31,63 @@ namespace used_car_dealer.ViewModels
             set => SetProperty(ref _customers, value);
         }
 
-        private string _name = "Imie";
+        private string _name;
         public string Name
         {
             get => _name;
             set => SetProperty(ref _name, value);
         }
 
-        private string _lastName = "Nazwisko";
+        private string _lastName;
         public string LastName
         {
             get => _lastName;
             set => SetProperty(ref _lastName, value);
         }
 
-        private string _pesel = "1234678910";
+        private string _pesel;
         public string Pesel
         {
             get => _pesel;
             set => SetProperty(ref _pesel, value);
         }
 
-        private string _phoneNumber = "123456789";
+        private string _phoneNumber;
         public string PhoneNumber
         {
             get => _phoneNumber;
             set => SetProperty(ref _phoneNumber, value);
         }
 
-        private string _postalCode = "12-345";
+        private string _postalCode;
         public string PostalCode
         {
             get => _postalCode;
             set => SetProperty(ref _postalCode, value);
         }
 
-        private string _city = "Testowe";
+        private string _city;
         public string City
         {
             get => _city;
             set => SetProperty(ref _city, value);
         }
 
-        private string _street = "Testowa";
+        private string _street;
         public string Street
         {
             get => _street;
             set => SetProperty(ref _street, value);
         }
 
-        private uint _homeNumber = 1;
+        private uint _homeNumber;
         public uint HomeNumber
         {
             get => _homeNumber;
             set => SetProperty(ref _homeNumber, value);
         }
 
-        private string _birthDate = "01.01.2000";
+        private string _birthDate;
         public string BirthDate
         {
             get => _birthDate;
@@ -150,7 +150,7 @@ namespace used_car_dealer.ViewModels
 
         public void EditCustomer()
         {
-            MessageBox.Show("Not implemented");
+
         }
         public void DeleteCustomer()
         {
